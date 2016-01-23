56257f985d5fbf1c96c5bcead27f10876949b8eb
Chance Shiflett <chanceshiflett@yahoo.com>
2016-01-23 15:42:50 -0800

    Add program that counts up to 100

---
+++ src/main/java/com/sqa/cs/basic/CountingUpTo100.java
@@ -0,0 +1,18 @@
+/**
+ * File Name: CountingUpTo100.java<br>
+ * Shiflett, Chance<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Jan 23, 2016
+ */
+package com.sqa.cs.basic;
+
+public class CountingUpTo100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 101; i++) {
+			System.out.println("Number " + i);
+		}
+	}
+}
