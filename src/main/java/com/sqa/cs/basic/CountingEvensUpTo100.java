9c5c15badfd416eafe5315dcd4521a7ac00e766e
Chance Shiflett <chanceshiflett@yahoo.com>
2016-01-23 15:49:40 -0800

    Add program that counts even numbers

---
+++ src/main/java/com/sqa/cs/basic/CountingEvensUpTo100.java
@@ -0,0 +1,24 @@
+/**
+ * File Name: CountingEvensUpTo100.java<br>
+ * Shiflett, Chance<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Jan 23, 2016
+ */
+package com.sqa.cs.basic;
+
+public class CountingEvensUpTo100 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int even;
+		for (int i = 0; i < 101; i++) {
+			even = i % 2;
+			if (even == 0) {
+				System.out.println("Number" + i);
+			}
+		}
+	}
+}
